8a0e761f888441a7ed26ddf309ce9b28ba6a198d
SSto <95592688+sstover25@users.noreply.github.com>
2022-04-18 15:25:41 -0500

    added CRUD API routes for Pizza

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
   {
     useNewUrlParser: true,
-    userUnifiedTopology: true,
+    useUnifiedTopology: true,
   }
 );
 
